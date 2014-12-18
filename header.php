b222c187a9e4a02a9071f8e0c4eedb53a3b6582a
DCanaan <Danny@MacBook-Air-de-Danny.local>
2014-12-18 08:49:35 +0100

    Upload photo de profil fonctionnel

---
+++ header.php
@@ -0,0 +1,30 @@
+<?php require_once("config.php"); ?>
+
+<div id="menu-wrapper">
+       
+        <div id="menu" class="container">
+       
+                <ul>
+       
+                        <li><a href="index.php"><img src="logo2.png" height="40" width="40" id="logo2"></a></li>
+                        <li><a href="#">Forum</a></li>
+                        <li><a href="logement.php">Logements</a></li>
+ 
+<?php
+       
+        if(!isConnected()) {
+                echo "<li><a href=\"inscription.php\">Inscription</a></li>";
+                echo "<li><a href=\"login.php\">Connexion</a></li>"; }
+ 
+        else {
+                echo "<li><a href=\"profil.php\">Profil</a></li>";
+                echo "<li><a href=\"logout.php\">Déconnexion</a></li>"; }
+ 
+?>
+ 
+                        <li><a href="index.php"><img src="logo2.png" height="40" width="40" id="logo2"></a></li>
+                </ul>
+       
+        </div>
+ 
+</div>
